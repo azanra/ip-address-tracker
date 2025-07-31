a643aeb7002c84da9af204e07b93637b6395816f
azan-personal <112457939+azanra@users.noreply.github.com>
2025-07-31 10:31:11 +0700

    Setup and render leaflet map

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,9 +1,11 @@
 import Info from "./Info";
+import LocationMap from "./LocationMap";
 
 const Body = ({ data }) => {
   return (
     <div>
       <Info data={data} />
+      <LocationMap data={data} />
     </div>
   );
 };
---
+++ src/component/LocationMap.jsx
@@ -0,0 +1,28 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const LocationMap = ({ data }) => {
+  const { location } = data;
+  const { lat, lng } = location;
+  return (
+    <div>
+      <MapContainer
+        center={[lat, lng]}
+        zoom={13}
+        style={{ height: "100vh", width: "100vw" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[lat, lng]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default LocationMap;
