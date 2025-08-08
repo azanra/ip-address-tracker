41a61d9e1db2f935b6d91cfa2bcf2f9f7f487b08
azan-personal <112457939+azanra@users.noreply.github.com>
2025-08-08 09:35:48 +0700

    Add img assets needed for the project

--- src/App.jsx
+++ src/App.jsx
@@ -17,39 +17,39 @@ function App() {
 
   const isFirstRender = useFirstRender();
 
-  const { data, loading, error } = useGetIpAddress(
-    searchKeyword,
-    isFirstRender
-  );
+  // const { data, loading, error } = useGetIpAddress(
+  //   searchKeyword,
+  //   isFirstRender
+  // );
 
-  // const data = {
-  //   ip: "8.8.8.8",
-  //   location: {
-  //     country: "US",
-  //     region: "California",
-  //     city: "Mountain View",
-  //     lat: 37.40599,
-  //     lng: -122.078514,
-  //     postalCode: "94043",
-  //     timezone: "-07:00",
-  //     geonameId: 5375481,
-  //   },
-  //   domains: [
-  //     "0d2.net",
-  //     "003725.com",
-  //     "0f6.b0094c.cn",
-  //     "007515.com",
-  //     "0guhi.jocose.cn",
-  //   ],
-  //   as: {
-  //     asn: 15169,
-  //     name: "Google LLC",
-  //     route: "8.8.8.0/24",
-  //     domain: "https://about.google/intl/en/",
-  //     type: "Content",
-  //   },
-  //   isp: "Google LLC",
-  // };
+  const data = {
+    ip: "8.8.8.8",
+    location: {
+      country: "US",
+      region: "California",
+      city: "Mountain View",
+      lat: 37.40599,
+      lng: -122.078514,
+      postalCode: "94043",
+      timezone: "-07:00",
+      geonameId: 5375481,
+    },
+    domains: [
+      "0d2.net",
+      "003725.com",
+      "0f6.b0094c.cn",
+      "007515.com",
+      "0guhi.jocose.cn",
+    ],
+    as: {
+      asn: 15169,
+      name: "Google LLC",
+      route: "8.8.8.0/24",
+      domain: "https://about.google/intl/en/",
+      type: "Content",
+    },
+    isp: "Google LLC",
+  };
 
   return (
     <div>
@@ -57,9 +57,10 @@ function App() {
       {keywordIsNotValid && searchKeyword.length > 0 && (
         <p>Not a valid ip address or url...</p>
       )}
-      {loading && <p>Loading the data...</p>}
+      {/* {loading && <p>Loading the data...</p>}
       {error && <p>Failed to fetch the data</p>}
-      {!loading && error === null && <Body data={data} />}
+      {!loading && error === null && <Body data={data} />} */}
+      <Body data={data} />
     </div>
   );
 }
