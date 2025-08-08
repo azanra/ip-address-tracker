75b43295c2bf34d1098d22a9b21ba7a27d640565
azan-personal <112457939+azanra@users.noreply.github.com>
2025-08-08 09:03:50 +0700

    Use ref to reduce unnecessary render and extract custom hook

---
+++ src/hooks/useFirstRender.js
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+
+function useFirstRender() {
+  const isFirstRender = useRef(true);
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+    }
+    console.log("first render to true !");
+    return () => {
+      isFirstRender.current = true;
+      console.log("Cleaning up first render to false");
+    };
+  }, []);
+
+  /* 
+    This is used to flag if its first render, because isFirstRender flag
+    is used to choose the url of api (external system), i think using effect 
+    is the correct answer here
+  */
+
+  return isFirstRender.current;
+}
+
+export default useFirstRender;
